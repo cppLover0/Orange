8e0429d4bf658ee41b9bf0a6d0c00f12b3e8830f
cpplover0 <osdev555@yandex.com>
2025-06-01 11:12:16 +0300

    bug fix

--- build-initrd.sh
+++ build-initrd.sh
@@ -3,12 +3,12 @@ CURRENT_DIR="$(realpath .)"
 
 export PATH="$HOME/opt/cross/orange/bin:$PATH"
 
-if [!$(which x86_64-orange-gcc)] 
-then
-		echo "It looks like you don't have cross-compiler, or you didn't add them to your PATH."; \
-		echo "If you built your cross compiler, add them to PATH with: "; \
-		echo 'export PATH="$$HOME/opt/cross/orange/bin:$$PATH"'; \
-		echo 'or build cross compiler with "make cross-compiler"'; \
+if [ ! "$(which x86_64-orange-gcc)" ]; then
+    echo "It looks like you don't have the cross-compiler installed, or it isn't in your PATH."
+    echo "If you built your cross-compiler, add it to your PATH with:"
+    echo 'export PATH="$HOME/opt/cross/orange/bin:$PATH"'
+    echo 'Alternatively, you can build the cross-compiler with: "make cross-compiler"'
+	exit 1
 fi
 
 rm -rf tools/orange-mlibc
